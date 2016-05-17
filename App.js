acc298a4c0b3ce31aced8338e079c5a6767fce25
Max Reno <maxreno@Maxs-MacBook-Air.local>
2016-05-17 11:11:13 -0400

    Added table that stores # and note

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = { };
+    this.state = {count: 1};
     this.update = this.update.bind(this);
   }
 
@@ -19,13 +19,17 @@ class App extends React.Component {
 //Add method for taking text from text box and adding to unordered list ul
   add(){
     var textToAdd = document.getElementById('textToAdd').value; //get text from text box
-    //if text isn't blank/null, add
+
     if(textToAdd!="") {
-      var ul = document.getElementById("list"); //get list
-      var li = document.createElement("li");    //make li element
-      li.appendChild(document.createTextNode(textToAdd));//add text node
-      ul.appendChild(li); //append li to list
-    }
+        var table = document.getElementById('table'); //get table from page
+        var row = table.insertRow(this.state.count);  //add blank row to table
+        var cell1 = row.insertCell(0);                //add blank cells to row
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = this.state.count;           //modify cell contents
+        cell2.innerHTML = textToAdd;
+        this.setState({count: this.state.count+1});   //increment state number
+      }
+      this.clear();                                 //clear textbox
   }
 
 //Clear method for clearing text out of text box before next button click
@@ -38,15 +42,24 @@ class App extends React.Component {
       <div>
         <input type="textarea" name="textToAdd" id="textToAdd" size="75" defaultValue={this.props.text} data-toggle="tooltip" data-placement="bottom" title="This is the new text we will add to the list!"/>
 
-        <button type="submit" class="btn btn-secondary" data-toggle="popover" data-container="body" data-placement="right" title="Add Item"
+        <button type="submit" className="btn btn-secondary" data-toggle="popover" data-container="body" data-placement="right" title="Add Item"
           data-content="This will add the item to the list!" data-trigger="hover" onClick={this.update.bind(this)}>
           Add Item!
         </button>
 
-        <br/> <br/> <br/> <br/>
+        <br/> <br/> <hr size="50"/> <br/>
 
-        <ul id="list"></ul>
-      </div>
+          <table data-toggle="table" className="table" id="table" >
+            <thead className="thead-inverse">
+              <tr>
+                <th>#</th>
+                <th>Note</th>
+              </tr>
+            </thead>
+            <tbody id="tbody">
+            </tbody>
+          </table>
+        </div>
     );
   }
 }
